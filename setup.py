5450a679a810c517bdf3b5f34b20c3b1ef53329b
sidaliu1014 <34602461+sidaliu1014@users.noreply.github.com>
2019-12-01 21:46:17 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='exch_etl',
+    version='1.0.0',
+    url='',
+    license='',
+    author='starstar',
+    author_email='sliu439@bloomberg.net',
+    description='Tool for China Exch ABS'
+)
